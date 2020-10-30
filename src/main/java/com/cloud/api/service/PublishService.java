0ae6b2670329f60c5ab2ce93a4e8e1d2078760db
亲爱的树树 <2230817302@qq.com>
2020-10-30 16:06:41 +0800

    保存发布任务信息

---
+++ src/main/java/com/cloud/api/service/PublishService.java
@@ -0,0 +1,11 @@
+package com.cloud.api.service;
+
+/**
+ * @author 成都犀牛
+ * @version version 1.0
+ * @date 2020/10/30 8:45
+ */
+public interface PublishService {
+
+    void saveReleaseInfo(String info);
+}
