55178beb68ea76982b08cda4fd0bad89c546ddf2
houdongsheng <3231894411@qq.com>
2020-11-09 19:36:13 +0800

    no message

--- src/test/java/com/cloud/api/EmailContent.java
+++ src/test/java/com/cloud/api/EmailContent.java
@@ -1,13 +1,2 @@
-package com.cloud.api;
 
-import com.cloud.api.mapper.VXUser.VXUserMapper;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-/**
- * @author 成都犀牛
- * @version version 1.0
- * @date 2020/10/26 22:26
- */
 
